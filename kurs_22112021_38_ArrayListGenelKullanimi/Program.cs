31d4f34aae04afcf23e0377bbe5350d85d986332
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-22 16:05:03 +0300

    Add project files.

---
+++ kurs_22112021_38_ArrayListGenelKullanimi/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace kurs_22112021_38_ArrayListGenelKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //10 elemanlı (string ) ArrayList içindeki Z-A olacak şekilde sıralayınız
+            ArrayList odevListe = new ArrayList();
+            odevListe.Add("Fatih");
+            odevListe.Add("Esra");
+            odevListe.Add("Ali Mert");
+            odevListe.Add("Serdar");
+            odevListe.Add("Gamze");
+            odevListe.Add("Hakan");
+            odevListe.Add("Furkan");
+            odevListe.Add("Birol");
+            odevListe.Add("Cem");
+            odevListe.Add("Yakup");
+            //tüm değerleri a-z çevir
+            odevListe.Sort(); //sıralama
+            //odevListe.Reverse(); //sıralananı tersten yazdırma.
+            foreach (var item in odevListe)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
